c9026beba92dcf32074afba83566a7d107eecb05
garenv <garenvartanian24@gmail.com>
2022-08-11 20:55:48 -0400

    made a few fixes on a few components

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Redis;
 
 class FileUploadController extends Controller
 {
