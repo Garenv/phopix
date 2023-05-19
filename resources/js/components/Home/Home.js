1df57565095f3a751ddb38689b6e4d7719a6f56f
garenv <garenvartanian24@gmail.com>
2023-05-19 16:16:28 -0400

    added .css to datepicker

--- resources/js/components/Home/Home.js
+++ resources/js/components/Home/Home.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import ApiClient from "../../utilities/ApiClient";
 import {toast, ToastContainer} from "react-toastify";
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const LoginRegister = () => {
     const [name, setName]                                           = useState("");
